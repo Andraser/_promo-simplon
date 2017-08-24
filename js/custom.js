2043df562479f9edcbe7cebc9087aca19e987080
Andraser <rockz-star@hotmail.co.uk>
2017-08-25 01:41:18 +0400

    ajout new button social network footer + scrollspy

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,49 @@
-window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop>1000||document.documentElement.scrollTop>1000){document.getElementById("myBtn").style.display="block";}else{document.getElementById("myBtn").style.display="none";}}function topFunction(){document.body.scrollTop=0;document.documentElement.scrollTop=0;}$(document).ready(function(){$.support.placeholder=(function(){var i=document.createElement('input');return'placeholder'in i;})();if($.support.placeholder){$('.form-label').each(function(){$(this).addClass('js-hide-label');});$('.form-group').find('input, textarea').on('keyup blur focus',function(e){var $this=$(this),$parent=$this.parent().find("label");if(e.type=='keyup'){if($this.val()==''){$parent.addClass('js-hide-label');}else{$parent.removeClass('js-hide-label');}}else if(e.type=='blur'){if($this.val()==''){$parent.addClass('js-hide-label');}else{$parent.removeClass('js-hide-label').addClass('js-unhighlight-label');}}else if(e.type=='focus'){if($this.val()!==''){$parent.removeClass('js-unhighlight-label');}}});}});
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+$(document).ready(function () {
+    $.support.placeholder = (function () {
+        var i = document.createElement('input');
+        return 'placeholder' in i;
+    })();
+    if ($.support.placeholder) {
+        $('.form-label').each(function () {
+            $(this).addClass('js-hide-label');
+        });
+        $('.form-group').find('input, textarea').on('keyup blur focus', function (e) {
+            var $this = $(this),
+                $parent = $this.parent().find("label");
+            if (e.type == 'keyup') {
+                if ($this.val() == '') {
+                    $parent.addClass('js-hide-label');
+                } else {
+                    $parent.removeClass('js-hide-label');
+                }
+            } else if (e.type == 'blur') {
+                if ($this.val() == '') {
+                    $parent.addClass('js-hide-label');
+                } else {
+                    $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
+                }
+            } else if (e.type == 'focus') {
+                if ($this.val() !== '') {
+                    $parent.removeClass('js-unhighlight-label');
+                }
+            }
+        });
+    }
+});
+
