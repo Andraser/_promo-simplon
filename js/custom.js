a6014c114e6f325338645cd4b0ad90a9c07e266d
Andraser <rockz-star@hotmail.co.uk>
2017-08-25 02:16:49 +0400

    correction bug

--- js/custom.js
+++ js/custom.js
@@ -26,20 +26,20 @@ $(document).ready(function () {
         $('.form-group').find('input, textarea').on('keyup blur focus', function (e) {
             var $this = $(this),
                 $parent = $this.parent().find("label");
-            if (e.type == 'keyup') {
-                if ($this.val() == '') {
+            if (e.type === 'keyup') {
+                if ($this.val() === '') {
                     $parent.addClass('js-hide-label');
                 } else {
                     $parent.removeClass('js-hide-label');
                 }
-            } else if (e.type == 'blur') {
-                if ($this.val() == '') {
+            } else if (e.type === 'blur') {
+                if ($this.val() === '') {
                     $parent.addClass('js-hide-label');
                 } else {
                     $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
                 }
-            } else if (e.type == 'focus') {
-                if ($this.val() !== '') {
+            } else if (e.type === 'focus') {
+                if ($this.val() !=== '') {
                     $parent.removeClass('js-unhighlight-label');
                 }
             }
